981890143cfbe23457be6dc455a8f2122168a70b
LAIBA SHEHZAD <ls2092@hw.ac.uk>
2025-07-15 00:39:23 +0500

    traffic sign

---
+++ Traffic_Sign_Predictor/main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras,utils import to_categorical
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import cv2 as cv
+import os
